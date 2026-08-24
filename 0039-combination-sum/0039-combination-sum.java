ff00a0b1e39b1cba464f0529ad935931f1073c86
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-08-24 22:35:26 +0530

    Time: 4 ms (12.99%), Space: 45.8 MB (51.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -4,7 +4,6 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
 
-        // Sorting helps us stop early when candidate > remaining target
         Arrays.sort(candidates);
 
         backtrack(candidates, target, 0, new ArrayList<>(), result);
@@ -19,7 +18,6 @@ class Solution {
             List<Integer> current,
             List<List<Integer>> result) {
 
-        // Target reached
         if (remaining == 0) {
             result.add(new ArrayList<>(current));
             return;
@@ -27,16 +25,13 @@ class Solution {
 
         for (int i = start; i < candidates.length; i++) {
 
-            // Since array is sorted, no later candidate can work
             if (candidates[i] > remaining) {
                 break;
             }
 
-            // Choose candidate
             current.add(candidates[i]);
 
-            // i, NOT i + 1:
-            // This allows the same number to be selected unlimited times.
+
             backtrack(
                 candidates,
                 remaining - candidates[i],
@@ -45,7 +40,6 @@ class Solution {
                 result
             );
 
-            // Backtrack
             current.remove(current.size() - 1);
         }
     }
