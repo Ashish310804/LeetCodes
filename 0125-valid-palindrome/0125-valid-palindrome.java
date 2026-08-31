0006bfc0b55e40d20c7fce6b1c0a104f2c634fa4
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-08-31 23:03:49 +0530

    Time: 2 ms (99.3%), Space: 44 MB (91.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -5,17 +5,17 @@ class Solution {
 
         while (left < right) {
 
-            // Skip non-alphanumeric characters from the left
+      
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
 
-            // Skip non-alphanumeric characters from the right
+      
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
 
-            // Compare characters ignoring case
+         
             if (Character.toLowerCase(s.charAt(left)) !=
                 Character.toLowerCase(s.charAt(right))) {
                 return false;
