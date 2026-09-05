c8aee0c41f81c9cdbfcd5c19cbec82d09b8df70a
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-09-05 23:24:42 +0530

    Time: 0 ms (100%), Space: 43.1 MB (51.28%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,14 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        // Skip trailing spaces by iterating backwards
         int i = s.length() - 1;
-        
-        // Step 1: Skip all trailing spaces
+
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
         
-        // Step 2: Count characters until we hit a space or beginning
         int length = 0;
         while (i >= 0 && s.charAt(i) != ' ') {
             length++;
